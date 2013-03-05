18d67c28fb45c777e67ae956ee97ad271ff0c787
Christian Kauhaus <kc@gocept.com>
2013-03-05 16:32:49 +0100

    Basic log output for /tmp/test

--- src/filelog/__init__.py
+++ src/filelog/__init__.py
@@ -1,4 +1,27 @@
-# Example package with a console entry point
+import datetime
+import json
+import pyinotify
+
+
+class EventHandler(pyinotify.ProcessEvent):
+
+    def process_default(self, event):
+        pathname = event.__dict__.pop('pathname')
+        maskname = event.__dict__.pop('maskname')
+        event.__dict__.pop('wd')
+        event.__dict__.pop('mask')
+        event.__dict__.pop('path')
+        print('{} {} {} {}'.format(
+            datetime.datetime.now().isoformat(),
+            pathname, maskname, json.dumps(event.__dict__)))
+
 
 def main():
-    print "Hello World"
+    handler = EventHandler()
+    mask = (pyinotify.IN_ATTRIB | pyinotify.IN_CLOSE_WRITE |
+            pyinotify.IN_CREATE | pyinotify.IN_DELETE |
+            pyinotify.IN_MOVED_FROM | pyinotify.IN_MOVED_TO)
+    wm = pyinotify.WatchManager()
+    notifier = pyinotify.Notifier(wm, handler)
+    wdd = wm.add_watch('/tmp/test', mask, rec=True, auto_add=True)
+    notifier.loop()
