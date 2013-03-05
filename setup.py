48f626a4848a30027797027d2f2a9ff3ac7b49f3
Christian Kauhaus <kc@gocept.com>
2013-03-05 18:52:21 +0100

    Add buildout

--- setup.py
+++ setup.py
@@ -1,3 +1,5 @@
+# python3.2
+
 from setuptools import setup, find_packages
 import os
 
@@ -7,18 +9,17 @@ here = os.path.abspath(os.path.dirname(__file__))
 version = '0.1'
 
 install_requires = [
-    # List your project dependencies here.
-    # For more details, see:
-    # http://packages.python.org/distribute/setuptools.html#declaring-dependencies
     'pyinotify>=0.9.4',
 ]
 
 
-setup(name='filelog',
+setup(
+    name='filelog',
     version=version,
     description="Log file changes in a directory",
     classifiers=[
-      # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+        'Programming Language :: Python :: 3.2',
+        'Environment :: Console',
     ],
     keywords='',
     author='Christian Kauhaus',
@@ -26,12 +27,11 @@ setup(name='filelog',
     url='',
     license='ZPL',
     packages=find_packages('src'),
-    package_dir = {'': 'src'},
+    package_dir={'': 'src'},
     include_package_data=True,
     zip_safe=False,
     install_requires=install_requires,
     entry_points={
-        'console_scripts':
-            ['filelog=filelog:main']
+        'console_scripts': ['filelog=filelog:main']
     }
 )
