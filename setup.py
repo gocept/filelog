ae3e8e14efc90f1a284b062506567a5f416bd71b
Christian Kauhaus <kc@gocept.com>
2013-03-06 11:19:51 +0100

    Complete metadata

--- setup.py
+++ setup.py
@@ -1,36 +1,34 @@
 # python3.2
 
 from setuptools import setup, find_packages
-import os
-
-here = os.path.abspath(os.path.dirname(__file__))
-
 
 version = '0.1'
-
-install_requires = [
-    'pyinotify>=0.9.4',
-]
-
 
 setup(
     name='filelog',
     version=version,
     description="Log file changes in a directory",
     classifiers=[
-        'Programming Language :: Python :: 3.2',
+        'Development Status :: 4 - Beta',
         'Environment :: Console',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: Zope Public License',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: 3.2',
+        'Topic :: System :: Systems Administration',
     ],
     keywords='',
     author='Christian Kauhaus',
     author_email='kc@gocept.com',
-    url='',
-    license='ZPL',
+    url='https://bitbucket.org/gocept/filelog',
+    license='ZPL-2.1',
     packages=find_packages('src'),
     package_dir={'': 'src'},
     include_package_data=True,
     zip_safe=False,
-    install_requires=install_requires,
+    install_requires=[
+        'pyinotify>=0.9.4',
+    ],
     entry_points={
         'console_scripts': ['filelog=filelog:main']
     }
